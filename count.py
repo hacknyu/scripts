f3b744df05ced09c92640558747899eae625d8cc
Michael Yang <themichaelyang@gmail.com>
2019-01-16 15:41:15 -0500

    Add count for minors

--- count.py
+++ count.py
@@ -9,18 +9,20 @@ def filter_dict(func, dictionary):
     filtered = filter(lambda item: func(*item), dictionary.items())
     return { key: value for key, value in filtered }
 
-def is_under_18(bday_str, event_date = date(2019, 2, 15)):
+def is_under_18(uid, user, event_date = date(2019, 2, 15)):
+    bday_str = user['birthDate']
     earliest_bday = date(event_date.year - 18, event_date.month, event_date.day)
     try:
         bday = parser.parse(bday_str).date()
         return bday > earliest_bday
     except Exception as e:
-        print(bday_str)
-        print(e)
+        print('> ' + str(e))
+        print('  ' + str(bday_str))
+        print('  uid: ' + str(uid))
         return True
 
 submitted = filter_dict(lambda uid, user: 'submitTimestamp' in user, data)
-under_18 = filter_dict(lambda uid, user: is_under_18(user['birthDate']), submitted)
+under_18 = filter_dict(lambda uid, user: is_under_18(uid, user), submitted)
 
 print('Total: ' + str(len(data)))
 print('Submitted: ' + str(len(submitted)))
