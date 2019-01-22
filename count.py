e7ad1cfb35e942a0c8df3c51f4addddd0b52c87b
Michael Yang <themichaelyang@gmail.com>
2019-01-22 11:52:21 -0500

    Added field response breakdowns

--- count.py
+++ count.py
@@ -1,7 +1,7 @@
 import json
 from datetime import timedelta, date
 from dateutil import parser
-
+from collections import defaultdict
 
 with open('data.json', 'r') as read_file:
     data = json.load(read_file)
@@ -22,6 +22,18 @@ def is_under_18(uid, form, event_date = date(2019, 2, 15)):
         print('  uid: ' + str(uid))
         return True
 
+def count_form_responses(form_key, users):
+    counts = defaultdict(int)
+    for uid, form in users.items():
+        for key, value in form.items():
+            if key == form_key:
+                counts[value] += 1
+    return counts
+
+def print_dict_counts(counts):
+    for key in counts:
+        print("  " + key + ": " + str(counts[key]))
+
 submitted = filter_dict(lambda uid, form: 'submitTimestamp' in form, data)
 
 nyu = filter_dict(lambda uid, form: 'nyuSchool' in form, submitted)
@@ -35,7 +47,8 @@ under_18 = filter_dict(lambda uid, form: is_under_18(uid, form), submitted)
 print('Total: ' + str(len(data)))
 print('Submitted: ' + str(len(submitted)))
 print('NYU: ' + str(len(nyu)))
-print('  Abu Dhabi: ' + str(len(abu_dhabi)))
-print('  Shanghai: ' + str(len(shanghai)))
+print_dict_counts(count_form_responses('nyuSchool', nyu))
+# print('  Abu Dhabi: ' + str(len(abu_dhabi)))
+# print('  Shanghai: ' + str(len(shanghai)))
 print('Postgrad: ' + str(len(postgrad)))
 print('Under 18: ' + str(len(under_18)))
